648bbdab964c6aa99fe9d195ddba15508fa3c0f3
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-10-26 19:05:10 +0100

    testing search artists

--- src/test/java/org/rookit/crawler/LastFMTest.java
+++ src/test/java/org/rookit/crawler/LastFMTest.java
@@ -1,9 +1,12 @@
 package org.rookit.crawler;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.number.OrderingComparison.*;
 
 import java.util.Set;
 
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.text.similarity.LevenshteinDistance;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.rookit.crawler.config.LastFMConfig;
@@ -22,11 +25,16 @@ public class LastFMTest {
 	private static MusicService lastFM;
 	private static DMTestFactory factory;
 	private static ArtistFactory artistFactory;
+	private static LevenshteinDistance distance;
+	private static LastFMConfig config;
 
 	@BeforeClass
 	public static void setUpBeforeClass() {
+		config = new LastFMConfig();
+		config.setLevenshteinThreshold(5);
+		distance = LevenshteinDistance.getDefaultInstance();
 		factory = DMTestFactory.getDefault();
-		lastFM = new LastFM(new LastFMConfig());
+		lastFM = new LastFM(config);
 		artistFactory = ArtistFactory.getDefault();
 	}
 
@@ -37,7 +45,7 @@ public class LastFMTest {
 		artists.add(artistFactory.createArtist(TypeArtist.GROUP, "Avicii"));
 		track.setMainArtists(artists);
 		final long count = lastFM.searchTrack(track)
-				.peek(t -> System.out.println(PrintUtils.track(t)))
+				//.peek(t -> System.out.println(PrintUtils.track(t)))
 				.count();
 		assertTrue(count > 0);
 	}
@@ -46,7 +54,19 @@ public class LastFMTest {
 	public final void testSearchArtistTracks() {
 		final Artist artist = artistFactory.createArtist(TypeArtist.GROUP, "Tritonal");
 		final long count = lastFM.searchArtistTracks(artist)
-				.peek(t -> System.out.println(PrintUtils.track(t)))
+				//.peek(t -> System.out.println(PrintUtils.track(t)))
+				.count();
+		assertTrue(count > 0);
+	}
+	
+	@Test
+	public final void testSearchArtist() {
+		final Artist artist = artistFactory.createArtist(TypeArtist.GROUP, "Green Day");
+		final long count = lastFM.searchArtist(artist)
+				.map(a -> Pair.of(a, distance.apply(a.getName().toLowerCase(), artist.getName().toLowerCase())))
+				.peek(a -> System.out.println(artist.getName() + "(" + a.getRight() + ")\n" + PrintUtils.artist(a.getLeft())))
+				.map(Pair::getRight)
+				.peek(score -> assertThat(score, lessThan(config.getLevenshteinThreshold())))
 				.count();
 		assertTrue(count > 0);
 	}

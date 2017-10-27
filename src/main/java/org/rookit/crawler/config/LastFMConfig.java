fe74bb06e693d58c6abee7a63188fd1e3b005b78
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-10-27 09:05:32 +0100

    debug config

--- src/main/java/org/rookit/crawler/config/LastFMConfig.java
+++ src/main/java/org/rookit/crawler/config/LastFMConfig.java
@@ -8,6 +8,7 @@ public class LastFMConfig {
 	private String apiKey;
 	private String user;
 	private int levenshteinThreshold;
+	private boolean debug;
 	
 	public String getApiKey() {
 		return getOrDefault(apiKey, "4c0824b3aa2f1114eee0ca8259512071");
@@ -26,6 +27,12 @@ public class LastFMConfig {
 	}
 	public void setLevenshteinThreshold(int levenshteinThreshold) {
 		this.levenshteinThreshold = levenshteinThreshold;
+	}
+	public boolean isDebug() {
+		return getOrDefault(debug, false);
+	}
+	public void setDebug(boolean debug) {
+		this.debug = debug;
 	}
 	
 	
